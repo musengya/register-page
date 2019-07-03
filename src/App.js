c42f57d197044a91555c0fe3a230e7f0a4aea941
Irene Mutua <mutuamirene@gmail.com>
2019-07-03 11:54:39 +0300

    Create a form

--- src/App.js
+++ src/App.js
@@ -1,1 +1,24 @@
+import React from "react";
 
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <form>
+          <label>FirstName</label>
+          <input type="text" placeholder="Ënter your firstname" />
+          <label>LastName</label>
+          <input type="text" placeholder="Ënter your lastname" />
+          <label>Email</label>
+          <input type="email" placeholder="Ënter your Email" />
+          <label>Password</label>
+          <input type="password" placeholder="Ënter your password" />
+          <label>Repeat Password</label>
+          <input type="password" placeholder="Repeat your password" />
+        </form>
+      </div>
+    )
+  }
+}
+
+export default App;
