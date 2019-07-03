180990bdda4d040690ccbfbd92c12b5c5ef18a45
Irene Mutua <mutuamirene@gmail.com>
2019-07-03 15:59:20 +0300

    Modified the file

--- src/App.js
+++ src/App.js
@@ -8,20 +8,20 @@ class App extends React.Component {
         <h2>Register Page</h2>
         <form>
           <label>FirstName</label>
-          <input type="text" placeholder="Ënter your firstname" />
-          <br />
+          <input type="text" placeholder="Enter your firstname" />
+          <br /><br />
           <label>LastName</label>
-          <input type="text" placeholder="Ënter your lastname" />
-          <br />
+          <input type="text" placeholder="Enter your lastname" />
+          <br /><br />
           <label>Email</label>
-          <input type="email" placeholder="Ënter your Email" />
-          <br />
+          <input type="email" placeholder="Enter your Email" />
+          <br /><br />
           <label>Password</label>
-          <input type="password" placeholder="Ënter your password" />
-          <br />
+          <input type="password" placeholder="Enter your password" />
+          <br /><br />
           <label>Repeat Password</label>
           <input type="password" placeholder="Repeat your password" />
-          <br />
+          <br /><br />
           <input type="submit" value="Submit" />
         </form>
       </div>
