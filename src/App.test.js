c2026d195188f6d89df0ef1340153c8a726c36ab
Irene Mutua <mutuamirene@gmail.com>
2019-07-09 16:10:03 +0300

    Add unit tests

--- src/App.test.js
+++ src/App.test.js
@@ -23,6 +23,31 @@ describe("App", () => {
 
     });
     test("displays an error when no value is input", () => {
- 
+        const { getByText } = render(<App />);
+        let username, mobileno, emailid, password;
+        if (username === null) {
+            expect(getByText("username").toThrow("*Please enter your username."))
+        }
+        if (mobileno === null) {
+            expect(getByText("mobileno").toThrow("*Please enter your mobile no."))
+        }
+        if (emailid === null) {
+            expect(getByText("emailid").toThrow("*Please enter your email-ID."))
+        }
+        if (password === null) {
+            expect(getByText("password").toThrow("*Please enter your password."))
+        }
     });
+    test("ensures the input meets the requirements", () => {
+        const { getByTestId } = render(<App />);
+        let username;
+        let expected = [
+            expect.stringMatching(/^[a-zA-Z ]*$/)
+        ]
+        if (username!==expected){
+            expect(getByTestId("user")).toThrow("*Please enter alphabet characters only.")
+            
+        }
+      
+    })
 })
